681afc219576b279aa71ea15d9d1dd729bba86f0
Bluieeee Yeoh <bluieeeeyeoh@Bluieeees-MacBook-Air.local>
2021-08-10 11:35:12 +0800

    day three build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   images: {
     domains: ["links.papareact.com"]
+  },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoiYmx1aWVlZWUiLCJhIjoiY2tzNWdlc3Q2MGlvNDJvcHRobHFkbTVpMCJ9.58YQbHVNcky1WZk-elI09A"
   }
 };
